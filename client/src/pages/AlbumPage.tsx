eeca399fd22300a212f3352aa7c3209c3a0cf98f
jacob tate <jacob11163@gmail.com>
2026-02-13 17:31:53 -0800

    FIX: Opening the wrong images

--- client/src/pages/AlbumPage.tsx
+++ client/src/pages/AlbumPage.tsx
@@ -83,7 +83,7 @@ export default function AlbumPage() {
     if (index !== -1) {
       lightbox.open(index);
     }
-  }, [imageParam, images.length]);
+  }, [imageParam, sortedImages]);
 
   if (loading) {
     return (
